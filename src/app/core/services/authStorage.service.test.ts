711222fcef0abfde8f8ee5084f1774342613b4ea
st-minhho <minh.ho@supremetech.vn>
2023-04-17 13:19:11 +0700

    update excercise 2

--- src/app/core/services/authStorage.service.test.ts
+++ src/app/core/services/authStorage.service.test.ts
@@ -1,25 +1,36 @@
 import { AuthStorageService } from './authStorage.service';
 
-describe('Set local storage item', () => {
-  const auth = new AuthStorageService();
+const auth = new AuthStorageService();
 
-  it('Check setToken', () => {
-    const setTokenMock = jest.spyOn(Storage.prototype, 'setItem');
-    auth.setToken('aaa');
+describe('Local storage set item', () => {
+  const setTokenMock = jest.spyOn(Storage.prototype, 'setItem');
+  auth.setToken('aaa');
+  it('Check setToken called', () => {
     expect(setTokenMock).toHaveBeenCalled();
+  });
+  it('Check setToken arguments', () => {
     expect(setTokenMock).toHaveBeenCalledWith('token', 'aaa');
   });
+});
 
-  it('Check getToken', () => {
-    const getTokenMock = jest.spyOn(Storage.prototype, 'getItem');
-    auth.getToken();
+describe('Local storage get item', () => {
+  const getTokenMock = jest.spyOn(Storage.prototype, 'getItem');
+  auth.getToken();
+  it('Check getToken called', () => {
     expect(getTokenMock).toHaveBeenCalled();
+  });
+  it('Check removeToken arguments', () => {
     expect(getTokenMock).toHaveBeenCalledWith('token');
   });
-  it('Check removeToken', () => {
-    const removeTokenMock = jest.spyOn(Storage.prototype, 'removeItem');
-    auth.removeToken();
+});
+
+describe('Local storage remove item', () => {
+  const removeTokenMock = jest.spyOn(Storage.prototype, 'removeItem');
+  auth.removeToken();
+  it('Check removeToken called', () => {
     expect(removeTokenMock).toHaveBeenCalled();
+  });
+  it('Check removeToken arguments', () => {
     expect(removeTokenMock).toHaveBeenCalledWith('token');
   });
 });
